de7157ec369cc9f73086df194812d65aa32e5196
hashaam13 <hashaamalam13@yahoo.com>
2022-10-12 17:13:53 +0500

    Add files via upload

---
+++ Neural Networks/untitled0.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 15 01:34:44 2021
+
+@author: hashaam
+"""
+import math
+
+import numpy as np
+a=np.ones((3,3),dtype=float)
+b=np.array([[1,2,3]])
+#s,ss=b.shape
+print(a[:,0:0+1])
